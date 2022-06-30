1897b8ae5501fee4e300b73e9310dbf354732e5b
thaflavorsaver <dojohnson@ucdavis.edu>
2022-06-29 20:31:48 -0700

    fixed error

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ var fs = require("fs");
 
 // Express app variables 
 var app = express();
-var PORT - process.env.PORT || 3001;
+var PORT = process.env.PORT || 3001;
 
 // Sets up the Express app to handle data parsing 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("puyblic"));
+app.use(express.static("public"));
