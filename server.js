c030290178498b096eab1cf438104a968273a33d
thaflavorsaver <dojohnson@ucdavis.edu>
2022-06-29 20:50:21 -0700

    added final get routes on server.js

--- server.js
+++ server.js
@@ -39,3 +39,59 @@ app.get("/notes", function (req, res) {
         });
     });
 });
+
+// Get route from db.json 
+app.get("/api/notes", function (req,res) {
+    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, data) {
+        if (error) {
+            return console.log(error)
+        }
+        console.log("this is Notes", data)
+        res.json(JSON.parse(data));
+    });
+});
+
+app.delete("/api/notes/:id", function (req, res) {
+    const noteId = JSON.parse(req.params.id)
+    console.log(noteId)
+    fs.readFile(__dirname + "/db/db.json", 'utf8', function (error, notes) {
+        if (error) {
+            return console.log(error)
+        }
+        notes = JSON.parse(notes)
+
+        notes = notes.filter(val => val.id !== noteId) 
+
+        fs.writeFile(__dirname + "/db/db.json", JSON.stringify(notes), function (error, data) {
+            if (error) {
+                return error
+            }
+            res.json(notes)
+        });
+    });
+});
+
+app.put("/api/notes/:id", function(req, res) {
+    const noteId = Json.parse(req.params.id)
+    console.log(noteId)
+    fs.readFile(__direname + "db/db.json", 'utf8', function(error, notes) {
+        if (error ){
+            return console.log(error)
+        }
+        notes.JSONparse(notes)
+
+        notes = notes.filter(val => val.id !== noteId)
+
+        fs.writeFile(__dirname +"db/db.json", JSON.stringify(notes), function (error, data) {
+            if (error) {
+              return error
+            }
+            res.json(notes)
+        });
+    });
+});
+
+// Start server
+app.listen(PORT, function () {
+    console.log("App listening on PORT " + PORT);
+  });
